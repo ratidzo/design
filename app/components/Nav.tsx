455a06e506cf992782e840a81eac04a7fa8f1e9a
ratidzo <ratygore@gmail.com>
2023-10-08 16:35:33 +0200

    dev: set responsive padding to left of ul element

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -38,7 +38,8 @@ export default function Nav() {
                 </div>
                 <ul className="md:flex md:items-center md:pb-0 pb-12 
                     absolute md:static bg-white md:z-auto z-[-1]
-                    left-0 w-full md:w-auto" >
+                    left-0 w-full md:w-auto md:pl-0 pl-9
+                    transition-all duration-500 ease-in" >
                     {
                         Links.map((link) => (
                             <li key={link.name}
